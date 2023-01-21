62018ad651e98ba89daad0b53ea02e4b31ff19ce
Dia'Monique Danner <developerdanner@Diamoniques-MacBook-Pro.local>
2023-01-21 14:53:01 -0500

    completed arrays example

---
+++ arrays-solution.js
@@ -0,0 +1,17 @@
+
+//creates array
+let array = [1,2,3,4,5];
+
+//loop through array, each element will always be printed in order!
+for(var i = 0; i < array.length; i++){
+    console.log(arr[i]);
+}
+
+//adds element to array
+array.push(6);
+console.log(arr);
+
+//removes last element from array
+array.pop()
+console.log(arr);
+
